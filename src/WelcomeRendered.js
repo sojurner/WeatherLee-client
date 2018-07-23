e4e0babcbb4813f4f48db758f4204e7747009c0f
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2018-07-23 15:18:33 -0600

    CSS layout changes to current weather

---
+++ src/WelcomeRendered.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+ const WelcomeRendered = (props) => {
+	return (
+  	<div className="welcome-rendered-title">
+      <h1 className="input-rendered-welcome">WeatherLy</h1>
+    </div>
+	)
+}
+
+export default WelcomeRendered;
