91ebc38314f11fb0c6581f6d4e68d5564c678499
sojurner <pykim0591@gmail.com>
2018-07-26 12:13:19 -0600

    indentation

--- src/Card.js
+++ src/Card.js
@@ -1,24 +1,24 @@
-import React from 'react';
+import React from "react";
 
 export const Card = props => {
-	if(props.condition) {
-		return (
-		<div className="seven-hour-cards">
-		<p className="seven-hour-time">{props.time}</p>
-		<p className="seven-hour-temperature">{props.temp}</p>
-		<p className="seven-hour-conditions">{props.condition}</p>
-		<img className="seven-hour-icon" src={props.icon_url} />
-		</div>
-		)
-} 
-	return (
-		<div className="ten-day-cards">
-			<p className="ten-day-day">{props.day}</p>
-			<p className="ten-day-date">{props.date}</p>
-			<p className="ten-day-high">{props.high}</p>
-			<p className="ten-day-low">{props.low}</p>
-			<img className="ten-day-icon" src={props.icon} />
-		</div>
-	)
-}
+  if (props.condition) {
+    return (
+      <div className="seven-hour-cards">
+        <p className="seven-hour-time">{props.time}</p>
+        <p className="seven-hour-temperature">{props.temp}</p>
+        <p className="seven-hour-conditions">{props.condition}</p>
+        <img className="seven-hour-icon" src={props.icon_url} />
+      </div>
+    );
+  }
+  return (
+    <div className="ten-day-cards">
+      <p className="ten-day-day">{props.day}</p>
+      <p className="ten-day-date">{props.date}</p>
+      <p className="ten-day-high">{props.high}</p>
+      <p className="ten-day-low">{props.low}</p>
+      <img className="ten-day-icon" src={props.icon} />
+    </div>
+  );
+};
 //indentation
