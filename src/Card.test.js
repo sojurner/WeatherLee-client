206f2eff3ef67da5b81aceddc5655d4a63ce2835
sojurner <pykim0591@gmail.com>
2018-07-26 12:01:35 -0600

    refactored CSS JS

--- src/Card.test.js
+++ src/Card.test.js
@@ -1,54 +1,53 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { mount } from 'enzyme';
 
 import { Card } from './Card'
 
 describe('Card', () =>{
-    let sevenHourWrapper;
-    let tenDayWrapper;
-    let sevenHourData = 
-    { key: 1,
-    time: '12:00 PM',
-    temp: '47°F',
-    condition: 'Partly Cloudy',
-    icon_url: 'http://icons.wxug.com/i/c/k/partlycloudy.gif' }
-    
+	let sevenHourWrapper;
+	let tenDayWrapper;
+	let sevenHourData = { 
+		key: 1,
+		time: '12:00 PM',
+		temp: '47°F',
+		condition: 'Partly Cloudy',
+		icon_url: 'http://icons.wxug.com/i/c/k/partlycloudy.gif' 
+	}
+	
+	let tenDayData = {
+		key: 1, 
+		day: 'Wednesday',
+		date: '12/20/2017',
+		high: '51°F',
+		low: '32°F',
+		icon: 'http://icons.wxug.com/i/c/k/partlycloudy.gif' 
+	}
 
-    let tenDayData = 
-        {
-        key: 1, 
-        day: 'Wednesday',
-        date: '12/20/2017',
-        high: '51°F',
-        low: '32°F',
-        icon: 'http://icons.wxug.com/i/c/k/partlycloudy.gif' 
-        }
+	beforeEach(() => {
+		sevenHourWrapper = mount(<Card
+			key={sevenHourData.key}
+			condition={sevenHourData.condition}
+			time={sevenHourData.time}
+			temp={sevenHourData.temp}
+			icon_url={sevenHourData.icon_url}    
+		/>)
+			
+		tenDayWrapper = mount(<Card 
+			key={tenDayData.key}
+			day={tenDayData.day}
+			date={tenDayData.high}
+			high={tenDayData.low}
+			icon={tenDayData.icon}
+		/>)
+	})
 
-    beforeEach(() => {
-        sevenHourWrapper = mount(<Card
-            key={sevenHourData.key}
-            condition={sevenHourData.condition}
-            time={sevenHourData.time}
-            temp={sevenHourData.temp}
-            icon_url={sevenHourData.icon_url}    
-        />)
-        
-        tenDayWrapper = mount(<Card 
-            key={tenDayData.key}
-            day={tenDayData.day}
-            date={tenDayData.high}
-            high={tenDayData.low}
-            icon={tenDayData.icon}
-        />)
-    })
+	it('should exist', () => {
+		expect(sevenHourWrapper).toBeDefined();
+		expect(tenDayWrapper).toBeDefined();
+	})
 
-    it('should exist', () => {
-        expect(sevenHourWrapper).toBeDefined();
-        expect(tenDayWrapper).toBeDefined();
-    })
-
-    it('should create seven hour and ten day cards', () => {
-        expect(sevenHourWrapper.find('p').length).toEqual(3)
-        expect(tenDayWrapper.find('p').length).toEqual(4)
-    })
+	it('should create seven hour and ten day cards', () => {
+		expect(sevenHourWrapper.find('p').length).toEqual(3)
+		expect(tenDayWrapper.find('p').length).toEqual(4)
+	})
 })
--- src/Search.test.js
+++ src/Search.test.js
@@ -4,40 +4,38 @@ import { shallow, mount } from 'enzyme';
 
 import Search from './Search'
 
-import App from './App'
-
 describe('Search component', ()=> {
-    let wrapper;
+  let wrapper;
 
-    beforeEach( () => {
-        wrapper = shallow(<Search />);
-    })
+  beforeEach( () => {
+		wrapper = shallow(<Search />);
+	})
 
-    it('shall render an input box and button when Search is rendered', () => {
-        expect(wrapper.find('input').length).toEqual(1)
-        expect(wrapper.find('button').length).toEqual(1)
-    })
+	it('shall render an input box and button when Search is rendered', () => {
+		expect(wrapper.find('input').length).toEqual(1)
+		expect(wrapper.find('button').length).toEqual(1)
+	})
 
-    it('should update State when input values are changed', () => {
-        let searchInput = wrapper.find('input')
+	it('should update State when input values are changed', () => {
+		let searchInput = wrapper.find('input')
 
-        let event = { target: {value: 'Denver'} };
+		let event = { target: {value: 'Denver'} };
 
-        searchInput.simulate('change', event)
+		searchInput.simulate('change', event)
 
-        expect(wrapper.state().location).toEqual('Denver')
-    })
+		expect(wrapper.state().location).toEqual('Denver')
+	})
 
-    it('should track event of button click', () => {
-        const mockCallBack = jest.fn();
+	it('should track event of button click', () => {
+		const mockCallBack = jest.fn();
 
-        let wrapperTwo = shallow(< Search setLocation={ () => mockCallBack() } />)
-        let inputValue = wrapperTwo.find('input')
-        console.log(inputValue)
-        let searchButton = wrapperTwo.find('button');
+		let wrapperTwo = shallow(< Search setLocation={ () => mockCallBack() } />)
+		let inputValue = wrapperTwo.find('input')
+		console.log(inputValue)
+		let searchButton = wrapperTwo.find('button');
 
-        searchButton.simulate('click');
-    
-        expect(mockCallBack).toHaveBeenCalled()
-    })
+		searchButton.simulate('click');
+
+		expect(mockCallBack).toHaveBeenCalled()
+	})
 })
