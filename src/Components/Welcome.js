270817f8c57388eb2ddd727bbe324453b13d9473
sojurner <pykim0591@gmail.com>
2018-10-21 14:52:48 -0600

    Shift rendering order of NavBar component to be before CurrentWeather component

--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -4,15 +4,14 @@ import { NavBar } from './NavBar';
 import { CurrentWeather } from './CurrentWeather';
 
 export const Welcome = ({ userLocation, current, openModal }) => {
-  console.log(userLocation);
   return !userLocation ? (
     <div className="welcome-title">
       <h1 className="input-welcome">WeatherLy</h1>
     </div>
   ) : (
     <div className="navigation">
-      <CurrentWeather currentWeather={current} />
       <NavBar openModal={openModal} />
+      <CurrentWeather currentWeather={current} userLocation={userLocation} />
     </div>
   );
 };
