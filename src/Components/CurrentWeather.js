4208aeec3eac0e098ca50c4a5dbff4efe749e309
sojurner <pykim0591@gmail.com>
2018-10-20 20:59:31 -0600

    Create individual p elements for each value for currentWeather

--- src/Components/CurrentWeather.js
+++ src/Components/CurrentWeather.js
@@ -1,15 +1,18 @@
-import React from "react";
+import React from 'react';
 
-export const CurrentWeather = props => {
+export const CurrentWeather = ({ currentWeather, userLocation }) => {
+  currentWeather;
   return (
     <div className="current-weather">
-      <p className="current-location">{props.currentWeather.location}</p>
-      <p className="current-time">{props.currentWeather.time}</p>
-      <p className="current-temp">{props.currentWeather.current}</p>
-      <p className="current-high">{props.currentWeather.high}</p>
-      <p className="current-low">{props.currentWeather.low}</p>
-      <p className="current-conditions">{props.currentWeather.conditions}</p>
-      <img className="current-icon" src={props.currentWeather.icon} />
+      <p className="current-location-1">{userLocation}</p>
+      <p className="current-time">{currentWeather.time}</p>
+      <p className="current-temp">{currentWeather.current}</p>
+      <p className="current-high">{currentWeather.high}</p>
+      <p className="current-low">{currentWeather.low}</p>
+      <p className="current-conditions">{currentWeather.conditions}</p>
+      <p className="current-sunrise">Sunrise: {currentWeather.sunriseTime}</p>
+      <p className="current-sunset">Sunset: {currentWeather.sunsetTime}</p>
+      <p className="current-precipitation">{currentWeather.precipitation}</p>
     </div>
   );
 };
