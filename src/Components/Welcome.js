e684ffd941d4d43e0e4e64d02e562cc94601769d
sojurner <pykim0591@gmail.com>
2018-10-21 11:05:41 -0600

    Modify Welcome component to conditionally render the NavBar and the CurrentWeather component

--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import '../CSS/Welcome.css';
+import { NavBar } from './NavBar';
+import { CurrentWeather } from './CurrentWeather';
 
-const Welcome = props => {
-  return (
+export const Welcome = ({ userLocation, current, openModal }) => {
+  console.log(userLocation);
+  return !userLocation ? (
     <div className="welcome-title">
       <h1 className="input-welcome">WeatherLy</h1>
+    </div>
+  ) : (
+    <div className="navigation">
+      <CurrentWeather currentWeather={current} />
+      <NavBar openModal={openModal} />
     </div>
   );
 };
